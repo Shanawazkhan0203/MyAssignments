fcf9d6af47e4713e4fe11e35c4a13f763a2f34bb
Shanawazkhan0203 <138970686+Shanawazkhan0203@users.noreply.github.com>
2025-03-18 00:11:09 +0530

    Add files via upload
    week1/day1

---
+++ day1/DataTypes.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		byte mybyte = 127 ;
+		short myshort = 32000;
+		int myInt = 252455 ;
+		long mylimit = 998876237 ;
+		float myfloat = 0.02478f ;
+		double mydouble = 0.023210145460 ;
+		char mycharLetter = 's';
+		boolean myboolValue = true ;
+		String name = "Shanawazkhan";
+		System.out.println(mybyte+ " "+myshort+" "+myInt+" "+mylimit+" "+myfloat+" "+mydouble+" "+mycharLetter+" "+myboolValue+" "+name);
+
+	}
+
+}
